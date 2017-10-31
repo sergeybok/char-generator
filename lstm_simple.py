a47df97ad1e881fcad6404d9c93ddac3ff26229b
Sergey Bokhnyak <sergeybok@gmail.com>
2017-10-31 13:59:47 +0100

    Did it without fixed sequence length, however only batch size of 1

--- lstm_simple.py
+++ lstm_simple.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 n_epochs = 20
-batch_size = 10
+batch_size = 1
 seq_len = 30
 
 learning_rate = 0.004
@@ -26,25 +26,31 @@ def load_data(dataset='will-to-power.txt'):
 	raw_text = raw_text.replace('  ', ' ')
 
 	data = list(raw_text)
-	data_i = [0]
+	data_i = []
+	cur_seq = [0]
 	m = 0
 	for c in data:
 		if (not c in dictionary):
 				dictionary.append(c)
 		if c == '.':
-			data_i.append(dictionary.index(c))
-			data_i.append(dictionary.index('END'))
-			data_i.append(dictionary.index('START'))
+			cur_seq.append(dictionary.index(c))
+			cur_seq.append(dictionary.index('END'))
+			data_i.append(cur_seq)
+			cur_seq = [0]
 			continue
-		data_i.append(dictionary.index(c))
+		cur_seq.append(dictionary.index(c))
 
-	d = np.zeros((len(data_i),len(dictionary)),dtype=np.float32)
-
+	#d = np.zeros((len(data_i),len(dictionary)),dtype=np.float32)
+	mx = len(dictionary)
 	for i in range(len(data_i)):
-		d[i][data_i[i]] = 1.0
+		seq = []
+		for j in range(len(data_i[i])):
+			feat = np.zeros((mx,),dtype=np.float32)
+			feat[data_i[i][j]] = 1.0
+			seq.append(feat)
+		data_i[i] = np.array(seq)
 
-
-	return d, len(dictionary)
+	return data_i, len(dictionary)
 
 
 print('loading data..')
@@ -55,8 +61,8 @@ d, mx = load_data()
 print('building model..')
 
 
-batchX = tf.placeholder(tf.float32,[batch_size, seq_len, mx],name='batchX')
-batchY = tf.placeholder(tf.float32,[batch_size, seq_len, mx],name='batchY')
+batchX = tf.placeholder(tf.float32,[None, None, mx],name='batchX') # [batchsize, seqlen, feature vector]
+batchY = tf.placeholder(tf.float32,[None, None, mx],name='batchY')
 
 lstm = tf.contrib.rnn.BasicLSTMCell(mx,state_is_tuple=False)
 
@@ -77,19 +83,25 @@ sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 
-n_batches = d.shape[0] // batch_size
+#n_batches = d.shape[0] // batch_size
+n_batches = len(d) // batch_size
 
 for epoch in range(n_epochs):
 	loss = []
-	for b in range(0,n_batches-batch_size-1,5):
+	for b in range(n_batches-batch_size-1):
+		x_in = [d[i][:-1] for i in range(batch_size*b,batch_size*(b+1))]
+		y_in = [d[i][1:] for i in range(batch_size*b,batch_size*(b+1))]
 		_, l = sess.run([train_step,ce_cost],feed_dict={
-												batchX:[d[i*seq_len:(i+1)*seq_len,:] for i in range(batch_size)],
-												batchY:[d[i*seq_len+1:(i+1)*seq_len+1,:] for i in range(batch_size)]})
+											batchX:x_in,
+											batchY:y_in})
 		loss.append(l.mean())
 
 	if epoch % 5:
 		avg_loss = sum(loss) / len(loss)
 		print('epoch = %i || loss = %i '%(epoch,avg_loss))
+
+
+
 
 
 
